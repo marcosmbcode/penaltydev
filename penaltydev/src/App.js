bbef85b04c720c80784cff4d2a3d9ede770c05d6
marcos corebiz <marcos.siqueira@corebiz.ag>
2020-09-16 22:36:23 -0300

    slider principal main

--- penaltydev/src/App.js
+++ penaltydev/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header/';
 import Footer from './components/Footer/';
+import Footer from './components/Slider/';
 import Main from './pages/main';
 import "./style.css";
 
@@ -9,6 +10,7 @@ const App = () => (
       <Header />
       <Main />
       <Footer />
+      <Slider />
     </div>
 );
  
